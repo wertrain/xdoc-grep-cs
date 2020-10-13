e72b5859a4eed4afaa942316ceb4b447d1a0b324
wertrain <wertrain@gmail.com>
2020-10-13 23:31:19 +0900

    WIP

---
+++ XDocGrep/FormMain.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace XDocGrep
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolTToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tableLayoutPanelMain = new System.Windows.Forms.TableLayoutPanel();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.panelMain = new System.Windows.Forms.Panel();
+            this.labelTarget = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBoxTarget = new System.Windows.Forms.ComboBox();
+            this.comboBoxExtensions = new System.Windows.Forms.ComboBox();
+            this.comboBoxSearchText = new System.Windows.Forms.ComboBox();
+            this.buttonTargetOpenFile = new System.Windows.Forms.Button();
+            this.buttonSearchTextOpenFile = new System.Windows.Forms.Button();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.tableLayoutPanelMain.SuspendLayout();
+            this.panelMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolTToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(899, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolTToolStripMenuItem
+            // 
+            this.toolTToolStripMenuItem.Name = "toolTToolStripMenuItem";
+            this.toolTToolStripMenuItem.Size = new System.Drawing.Size(55, 20);
+            this.toolTToolStripMenuItem.Text = "Tool(&T)";
+            // 
+            // tableLayoutPanelMain
+            // 
+            this.tableLayoutPanelMain.ColumnCount = 1;
+            this.tableLayoutPanelMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanelMain.Controls.Add(this.progressBar, 0, 1);
+            this.tableLayoutPanelMain.Controls.Add(this.panelMain, 0, 0);
+            this.tableLayoutPanelMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanelMain.Location = new System.Drawing.Point(0, 24);
+            this.tableLayoutPanelMain.Name = "tableLayoutPanelMain";
+            this.tableLayoutPanelMain.RowCount = 2;
+            this.tableLayoutPanelMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanelMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 27F));
+            this.tableLayoutPanelMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanelMain.Size = new System.Drawing.Size(899, 151);
+            this.tableLayoutPanelMain.TabIndex = 1;
+            // 
+            // progressBar
+            // 
+            this.progressBar.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.progressBar.Location = new System.Drawing.Point(3, 127);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(893, 21);
+            this.progressBar.TabIndex = 0;
+            // 
+            // panelMain
+            // 
+            this.panelMain.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelMain.Controls.Add(this.buttonSearch);
+            this.panelMain.Controls.Add(this.buttonSearchTextOpenFile);
+            this.panelMain.Controls.Add(this.buttonTargetOpenFile);
+            this.panelMain.Controls.Add(this.comboBoxSearchText);
+            this.panelMain.Controls.Add(this.comboBoxExtensions);
+            this.panelMain.Controls.Add(this.comboBoxTarget);
+            this.panelMain.Controls.Add(this.label2);
+            this.panelMain.Controls.Add(this.label1);
+            this.panelMain.Controls.Add(this.labelTarget);
+            this.panelMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelMain.Location = new System.Drawing.Point(3, 3);
+            this.panelMain.Name = "panelMain";
+            this.panelMain.Size = new System.Drawing.Size(893, 118);
+            this.panelMain.TabIndex = 1;
+            // 
+            // labelTarget
+            // 
+            this.labelTarget.AutoSize = true;
+            this.labelTarget.Location = new System.Drawing.Point(8, 12);
+            this.labelTarget.Name = "labelTarget";
+            this.labelTarget.Size = new System.Drawing.Size(144, 12);
+            this.labelTarget.TabIndex = 0;
+            this.labelTarget.Text = "Search File / Directory (&A)";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Target Extensions (&E)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Search Text (&S)";
+            // 
+            // comboBoxTarget
+            // 
+            this.comboBoxTarget.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBoxTarget.FormattingEnabled = true;
+            this.comboBoxTarget.Location = new System.Drawing.Point(158, 9);
+            this.comboBoxTarget.Name = "comboBoxTarget";
+            this.comboBoxTarget.Size = new System.Drawing.Size(682, 20);
+            this.comboBoxTarget.TabIndex = 3;
+            // 
+            // comboBoxExtensions
+            // 
+            this.comboBoxExtensions.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBoxExtensions.FormattingEnabled = true;
+            this.comboBoxExtensions.Location = new System.Drawing.Point(158, 34);
+            this.comboBoxExtensions.Name = "comboBoxExtensions";
+            this.comboBoxExtensions.Size = new System.Drawing.Size(682, 20);
+            this.comboBoxExtensions.TabIndex = 4;
+            // 
+            // comboBoxSearchText
+            // 
+            this.comboBoxSearchText.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBoxSearchText.FormattingEnabled = true;
+            this.comboBoxSearchText.Location = new System.Drawing.Point(158, 59);
+            this.comboBoxSearchText.Name = "comboBoxSearchText";
+            this.comboBoxSearchText.Size = new System.Drawing.Size(682, 20);
+            this.comboBoxSearchText.TabIndex = 5;
+            // 
+            // buttonTargetOpenFile
+            // 
+            this.buttonTargetOpenFile.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonTargetOpenFile.Location = new System.Drawing.Point(846, 7);
+            this.buttonTargetOpenFile.Name = "buttonTargetOpenFile";
+            this.buttonTargetOpenFile.Size = new System.Drawing.Size(37, 23);
+            this.buttonTargetOpenFile.TabIndex = 6;
+            this.buttonTargetOpenFile.Text = "...";
+            this.buttonTargetOpenFile.UseVisualStyleBackColor = true;
+            // 
+            // buttonSearchTextOpenFile
+            // 
+            this.buttonSearchTextOpenFile.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonSearchTextOpenFile.Location = new System.Drawing.Point(846, 57);
+            this.buttonSearchTextOpenFile.Name = "buttonSearchTextOpenFile";
+            this.buttonSearchTextOpenFile.Size = new System.Drawing.Size(37, 23);
+            this.buttonSearchTextOpenFile.TabIndex = 7;
+            this.buttonSearchTextOpenFile.Text = "...";
+            this.buttonSearchTextOpenFile.UseVisualStyleBackColor = true;
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Location = new System.Drawing.Point(684, 86);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(199, 23);
+            this.buttonSearch.TabIndex = 8;
+            this.buttonSearch.Text = "Search (&F)";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(899, 175);
+            this.Controls.Add(this.tableLayoutPanelMain);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FormMain";
+            this.Text = "XGrepper";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.tableLayoutPanelMain.ResumeLayout(false);
+            this.panelMain.ResumeLayout(false);
+            this.panelMain.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolTToolStripMenuItem;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanelMain;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Panel panelMain;
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.Button buttonSearchTextOpenFile;
+        private System.Windows.Forms.Button buttonTargetOpenFile;
+        private System.Windows.Forms.ComboBox comboBoxSearchText;
+        private System.Windows.Forms.ComboBox comboBoxExtensions;
+        private System.Windows.Forms.ComboBox comboBoxTarget;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelTarget;
+    }
+}
+
